8c359e02d2e823d1f588f3b74ce58da488820e81
Rene Garcia <rene.saaenz@gmail.com>
2019-11-21 16:01:53 -0600

    Implements get next option

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -3,4 +3,11 @@ class Poll < ApplicationRecord
   has_many :options, dependent: :destroy
   has_and_belongs_to_many :users
   enum status: [:pending, :done]
+
+  def getNextOption(id = nil)
+    options = self.options
+    return nil if options.first.nil?
+    return options.first.id if id == nil
+    options.where("id > ?", id).order("id ASC").first.id || options.first.id
+  end
 end
