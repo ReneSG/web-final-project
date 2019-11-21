0ed17b1086768d6c5862a83d1f06bc9fcfd84c50
Rene Garcia <rene.saaenz@gmail.com>
2019-11-21 13:54:50 -0600

    Adds register response endpoint

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -5,8 +5,28 @@ class OptionsController < ApplicationController
     @option_info = @client.spot(@option.place_id)
     min_photos = [5, @option_info.photos.count].min
     @photos = []
+    return if min_photos == 0
     (0..min_photos).each do |i|
       @photos << @option_info.photos[i].fetch_url(800)
+    end
+  end
+
+  def register_response
+    @option = Option.find(params[:id])
+    if params[:vote_yes] == "true"
+      @option.positive_votes += 1
+    else
+      @option.negative_votes += 1
+    end
+
+    if @option.save
+      respond_to do |format|
+        format.html { redirect_to option_answer_path(@option.id + 1)}
+      end
+    else
+      respond_to do |format|
+        format.html { render root }
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :polls
   devise_for :users
-  get "options/answer/:id", to: "options#answer"
+  get "options/answer/:id", as: "option_answer", to: "options#answer"
+  post "options/register_response/:id", as: "option_response", to: "options#register_response"
   root to: "polls#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
