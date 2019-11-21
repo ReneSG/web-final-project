859d35f7bf56ec5e9105d89569dffab08f7ce8ca
Rene Garcia <rene.saaenz@gmail.com>
2019-11-21 09:43:29 -0600

    Adds status column to polls

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,4 +2,5 @@ class Poll < ApplicationRecord
   belongs_to :owner, class_name: :User, foreign_key: :owner_id
   has_many :options, dependent: :destroy
   has_and_belongs_to_many :users
+  enum status: [:pending, :done]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191118034740) do
+ActiveRecord::Schema.define(version: 20191121153841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20191118034740) do
     t.integer "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "polls_users", id: false, force: :cascade do |t|
